e1d20af2ddec5b1fdd4aab557d462f42eba5dcec
adandach <ahmad.dandachi@gmail.com>
2018-03-01 15:27:53 -0500

    Simplify test

--- src/com/ford/smarthome/switches/Switch9000Test.java
+++ src/com/ford/smarthome/switches/Switch9000Test.java
@@ -3,26 +3,13 @@ package com.ford.smarthome.switches;
 import com.ford.smarthome.switchable.LightBulb2300;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class Switch9000Test {
 
     @Test
-    public void pressTurnsTheSwitchOn() {
-        Switch9000 switch9000 = new Switch9000(new LightBulb2300());
-
-        switch9000.press();
-
-        assertTrue(switch9000.isOn());
-    }
-
-    @Test
-    public void pressTwiceTurnsTheSwitchOff() {
+    public void pressSwitch() {
         Switch9000 switch9000 = new Switch9000(new LightBulb2300());
 
         switch9000.press();
         switch9000.press();
-
-        assertFalse(switch9000.isOn());
     }
 }
